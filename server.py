1a9c6727399da732f8d9aafa3f3bfa04fbd9ce4d
DESKTOP-R4KNH9C\Tofunmi <tofunmiseun@gmail.com>
2017-12-07 11:43:27 +0100

    fixed bug

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
 from flask import Flask, send_file
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 
 @app.route('/index.html')
 def serve_index_page():
     return send_file('index.html')
 
+
 @app.route('/add-tribute.html')
 def serve_tribute_page():
     return send_file('add-tribute.html')
 
+
 @app.route('/')
 def home():
-    serve_index_page()
+    return serve_index_page()
+
 
 if __name__ == '__main__':
     app.run('127.0.0.1', 1967, True)
