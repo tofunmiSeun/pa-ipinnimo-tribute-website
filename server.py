21d5ef5086a4ae645879bd3957ae2e108b3fed66
DESKTOP-R4KNH9C\Tofunmi <tofunmiseun@gmail.com>
2017-12-07 21:10:31 +0100

    added endpoints for all tributes and add new tribute

--- server.py
+++ server.py
@@ -1,6 +1,29 @@
-from flask import Flask, send_file
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+from logic.tributeLogic import TributeLogic
 
 app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api/tribute', methods=['POST'])
+def add_new_tribute():
+    try:
+        tribute_json_object = request.json
+        if tribute_json_object:
+            TributeLogic.new_tribute(tribute_json_object)
+
+    except Exception as exception_object:
+        print(exception_object)
+        return "Internal Server error", 500, {'ContentType': 'application/json'}
+
+    return "", 200, {'ContentType': 'application/json'}
+
+
+@app.route('/api/tributes/all')
+def get_all_tributes():
+    all_tributes = TributeLogic.get_all_tributes()
+    return jsonify([tribute.json_format for tribute in all_tributes])
 
 
 @app.route('/index.html')
