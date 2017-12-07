b0202ce5eddb704cb1669693a375db0dbbb33329
DESKTOP-R4KNH9C\Tofunmi <tofunmiseun@gmail.com>
2017-12-07 22:34:03 +0100

    removed unused carousel. changed db endpoints.

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,3 @@
 class Constants:
-    DB_CONNECTION_URL = 'mongodb://localhost:27017/'
-    DB_NAME = 'pa-ipinimo-funeral-webapp'
+    DB_CONNECTION_URL = 'mongodb://funs:neral@ds033196.mlab.com:33196/funeral-tribute'
+    DB_NAME = 'funeral-tribute'
