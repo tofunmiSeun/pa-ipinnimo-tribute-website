9c8f58174b5426c605bb56685981a7509ee82a92
DESKTOP-R4KNH9C\Tofunmi <tofunmiseun@gmail.com>
2017-12-07 22:48:34 +0100

    added image for carousel.

--- static/app/main.js
+++ static/app/main.js
@@ -1,5 +1,5 @@
 var app = angular.module('funeral-tribute', ['ui.router']);
-app.constant('SERVER_URL', 'http://localhost:1967/api');
+app.constant('SERVER_URL', 'https://pa-ipinimo-tribute.herokuapp.com/api');
 
 /*
 app.service('Session', function () {
